2d3c320b3321c33cfcdae2282364580d67bfab45
Markus Speckle <maex.sp@gmail.com>
2019-10-25 17:08:46 +0200

    demo to show vector based font icons

--- XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
 namespace XamarinApp
---
+++ XamarinApp/XamarinApp/XamarinApp/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public class ViewModel : BaseViewModel
+    {
+        int fontSize = 64;
+        bool isIconBackgroundOn = true;
+
+        public ViewModel()
+        {
+            Title = "Icons";
+            IncreaseFontSizeCommand = new Command(() =>
+            {
+                if (FontSize >= 72) return;
+                FontSize += 2;
+            });
+            DecreaseFontSizeCommand = new Command(() =>
+            {
+                if (FontSize <= 6) return;
+                FontSize -= 2;
+            });
+        }
+
+        public int FontSize
+        {
+            get { return fontSize; }
+            set { SetProperty(ref fontSize, value); }
+        }
+
+        public bool IsIconBackgroundOn
+        {
+            get { return isIconBackgroundOn; }
+            set { SetProperty(ref isIconBackgroundOn, value); }
+        }
+
+        public ICommand IncreaseFontSizeCommand { get; set; }
+
+        public ICommand DecreaseFontSizeCommand { get; set; }
+    }
+}
