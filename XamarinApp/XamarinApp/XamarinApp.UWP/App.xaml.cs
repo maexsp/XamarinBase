4085ec86e246d9faf52eba437dad79ee4a006af0
Markus Speckle <maex.sp@gmail.com>
2019-12-04 17:35:44 +0100

    MasterDetailPage with issue of Entry Text selected and menu bar slides in

--- XamarinApp/XamarinApp/XamarinApp.UWP/App.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp.UWP/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using UnhandledExceptionEventArgs = Windows.UI.Xaml.UnhandledExceptionEventArgs;
 
 namespace XamarinApp.UWP
 {
@@ -39,8 +40,6 @@ namespace XamarinApp.UWP
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-
-
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
--- XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace XamarinApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
